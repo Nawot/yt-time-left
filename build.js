d1a01a5ee8601e83a569f5ebe7d65c7f12d28026
Nawot <Nawot001@gmail.com>
2021-10-10 13:17:29 +0300

    Changed build.js. Excluding some files or dirs from zip for upload to AMO

--- build.js
+++ build.js
@@ -1,12 +1,19 @@
 var admZip = require('adm-zip')
 
-
+let ignore_prefix = '#'
 let path = 
 {
     build: 'dist/',
     src: 'src/'
 }
 let zip = new admZip()
-zip.addLocalFolder(path.src)
+zip.addLocalFolder(path.src, null,
+    function(filepath)
+    {
+        let fp = filepath.split(/[/|\\]+/) // split by / or \
+        for(dir of fp)
+            if(dir[0] == ignore_prefix) {return false}
+        return true
+    })
 zip.writeZip(path.build + 'yt-time-left.zip');
 
